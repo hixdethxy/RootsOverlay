cf134bb109db65fc62ff438c70917fede920393b
hixdethxy <j26395913@gmail.com>
2026-05-27 10:46:25 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Roots Overlay
 // @namespace    http://tampermonkey.net/
-// @version      1.4
+// @version      1.7
 // @description  root's overlay
 // @author       Root
 // @match        *://*.jklm.fun/*
